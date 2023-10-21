ccf621554275471666df1eeebabb57004370a253
Pedro Serey <serey.pedro@gmail.com>
2023-10-21 16:53:00 -0300

    add: aplicability importance change in chi-squared tests

--- hypotesis_test.r
+++ hypotesis_test.r
@@ -5,12 +5,10 @@ survey <- read.csv("./pp_survey_v2.csv")
 data <- subset(survey, EC != "ND")
 data$EC <- ifelse(data$EC == "MT", 1, 0)
 
-hist(data$IES)
-hist(data$ICA)
-
-model <- glm(EC ~ ICA + IES, data = data, family = "binomial")
-ies_chi <- chisq.test(data$ICA, data$EC)
-ica_chi <- chisq.test(data$IES, data$EC)
+# model <- glm(EC ~ ICA + IES, data = data, family = "binomial")
+model <- glm(EC ~ IAES + IACA, data = data, family = "binomial")
+ies_chi <- chisq.test(data$IACA, data$EC)
+ica_chi <- chisq.test(data$IAES, data$EC)
 
 print(summary(model))
 print(ies_chi)
